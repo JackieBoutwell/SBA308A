1895cad3c7a9ac5e06718c61050654bb74fd59c2
Jackie Boutwell <boutwelljackie983@gmail.com>
2024-04-16 20:52:54 -0600

    moved get and put in new file

---
+++ put.js
@@ -0,0 +1,20 @@
+export const uploadJson = async (fileContent) => {
+  const url = 'https://json-store.p.rapidapi.com/';
+  const options = {
+    method: 'PUT',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '818816d76fmshec5f96e5bcd9cd6p121067jsn3e4c582a5cf7',
+      'X-RapidAPI-Host': 'json-store.p.rapidapi.com'
+    },
+    body: '{ "name": "Example", "document": ' + fileContent + ' }'
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
--- script.js
+++ script.js
@@ -1,71 +1,10 @@
-document.getElementById("search-btn").addEventListener("click", function() {
+import { currentConditions } from "./get";
+import { uploadJson } from "./put";
+
+document.getElementById("search-btn").addEventListener("click", function () {
     let zipCode = document.getElementById("search-input").value;
     currentConditions(zipCode);
 }, false);
-
-const currentConditions = async (zipCode) => {
-  try {
-    // const headers = new Headers({
-    //   'x-api-key': 'A0eLxjDMQC919yA8VukwvzicrL5uZqGk',
-    // });
-    const response = await fetch(
-      `http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=A0eLxjDMQC919yA8VukwvzicrL5uZqGk&q=${zipCode}`
-    );
-      const data = await response.json();
-    const locationKey = data[0].ParentCity.Key;
-
-    const fiveDayForecastResponse = await fetch(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=A0eLxjDMQC919yA8VukwvzicrL5uZqGk`);
-    const fiveDayForecast = await fiveDayForecastResponse.json();
-      
-    //document.getElementById("greeting").innerHTML = "";
-      const dataTable = document.getElementById("data-table");
-      dataTable.innerHTML = "";
-    const city = document.createElement("h1").innerHTML = data[0].LocalizedName + " weather forecast for the next five days";
-    dataTable.append(city);
-      
-      const weatherTable = document.createElement("table");
-      weatherTable.classList.add("weather-table");
-    //   weatherTable.style.width = "90%";
-    //   weatherTable.style.margin = "auto";
-    //   weatherTable.style.textAlign = "center";
-      const tableHeaders = document.createElement("tr");
-      const dateHeader = document.createElement("th");
-      const forecastHeader = document.createElement("th");
-      const tempHeader = document.createElement("th");
-      //const windHeader = document.createElement("th");
-      dateHeader.innerHTML = "Date";
-      forecastHeader.innerHTML = "Forecast";
-      tempHeader.innerHTML = "Temperature";
-      //windHeader.innerHTML = "Wind Speed";
-      tableHeaders.appendChild(dateHeader);
-      tableHeaders.appendChild(forecastHeader);
-      tableHeaders.appendChild(tempHeader);
-      //tableHeaders.appendChild(windHeader);
-      weatherTable.appendChild(tableHeaders);
-      dataTable.appendChild(weatherTable);
-      console.log(fiveDayForecast);
-      fiveDayForecast.DailyForecasts.forEach(dailyForecast => {
-          let row = document.createElement("tr");
-          let date = document.createElement("td");
-          let forecast = document.createElement("td");
-          let temp = document.createElement("td");
-          //let wind = document.createElement("td");
-
-          date.innerHTML = new Date(dailyForecast["Date"]).toDateString();
-          forecast.innerHTML = dailyForecast["Day"]["IconPhrase"];
-          temp.innerHTML = "Max: " + dailyForecast["Temperature"]["Maximum"]["Value"] + " Min: " + dailyForecast["Temperature"]["Minimum"]["Value"];
-          row.appendChild(date);
-          row.appendChild(forecast);
-          row.appendChild(temp);
-          weatherTable.appendChild(row);
-        console.log(dailyForecast);
-    });
-
-  } catch (error) {
-    console.log('error ', error);
-  }
-};
-
 
 // PUT request 
 document.getElementById("json-upload").addEventListener("change", function () {
@@ -77,27 +16,6 @@ document.getElementById("json-upload").addEventListener("change", function () {
   };
   reader.readAsText(importedFile);
 }, false);
-
-const uploadJson = async (fileContent) => {
-  const url = 'https://json-store.p.rapidapi.com/';
-  const options = {
-    method: 'PUT',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': '818816d76fmshec5f96e5bcd9cd6p121067jsn3e4c582a5cf7',
-      'X-RapidAPI-Host': 'json-store.p.rapidapi.com'
-    },
-    body: '{ "name": "Example", "document": ' + fileContent + ' }'
-  };
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 // import axios from "axios";
 
